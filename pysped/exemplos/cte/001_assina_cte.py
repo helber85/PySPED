45d620f0113a36708c32853b00566fb290931b11
alanjds <alanjds@ts006.ts>
2012-12-18 14:49:41 -0200

    Exemplo usa um arquivo real && Limpar espaços em branco

--- pysped/exemplos/cte/001_assina_cte.py
+++ pysped/exemplos/cte/001_assina_cte.py
@@ -12,10 +12,8 @@ if __name__ == '__main__':
     certificado.arquivo = open(FILE_DIR+'/certificado_caminho.txt').read().strip()
     # arquivo 'certificado_senha.txt' deve conter a senha para o 'certificado.pfx'
     certificado.senha = open(FILE_DIR+'/certificado_senha.txt').read().strip()
-    salvar_arquivos = True
-
+    
     cte_xml = open(FILE_DIR+'/xml_teste/000000246.xml', 'r').read()
-
     xml_assinado = certificado.assina_xml(cte_xml)
 
     print xml_assinado
