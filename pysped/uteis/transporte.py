7ca5d2f6cbe559a84b69cb7e2c0bde405b6a7e05
Alan Justino <alan.justino@yahoo.com.br>
2012-12-18 19:43:22 -0200

    Whitespace & Code Style

--- pysped/uteis/transporte.py
+++ pysped/uteis/transporte.py
@@ -13,15 +13,15 @@ class HTTPSCertAuthHandler(urllib2.HTTPSHandler):
     def __init__(self, key, cert):  
         urllib2.HTTPSHandler.__init__(self)  
         self.key = key  
-        self.cert = cert  
-
+        self.cert = cert
+        
     def https_open(self, req):  
         #Rather than pass in a reference to a connection class, we pass in  
         # a reference to a function which, for all intents and purposes,  
-        # will behave as a constructor 
+        # will behave as a constructor
         return self.do_open(self.getConnection, req) 
 
-    def getConnection(self, host, timeout=300):  
+    def getConnection(self, host, timeout=300):
         return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)  
 
 class HttpsCertTransport(HttpTransport):
@@ -57,9 +57,9 @@ class HttpsCertTransport(HttpTransport):
         @rtype: fp
         """
         tm = self.options.timeout
-        url = urllib2.build_opener(HTTPSCertAuthHandler(self.key, self.cert))  
+        url = urllib2.build_opener(HTTPSCertAuthHandler(self.key, self.cert))
+
         connection = url.open(request, timeout=tm)
-        
         return connection
 
 if __name__ == '__main__':
@@ -77,8 +77,9 @@ if __name__ == '__main__':
     cert_path = os.environ.get('SPED_CERT_PATH', key_path) # Juntos KEY e CERT?
     test_url = 'https://nfe.fazenda.sp.gov.br/cteWEB/services/cteStatusServico.asmx'
 
+    transport = HttpsCertTransport(key_path, cert_path, password=key_password)
     c = Client(test_url,
-        transport = HttpsCertTransport(key_path, cert_path)
+        transport = transport
     )
 
     print c
